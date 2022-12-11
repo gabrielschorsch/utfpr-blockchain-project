406b51d5b5d2df5bec3d1e414f9a9da745a02970
Gabriel Schorsch <gabrielschorschgsa@gmail.com>
2022-12-11 14:48:08 -0300

    restart mining working

--- main.c
+++ main.c
@@ -29,7 +29,7 @@ BlocoNaoMinerado createBlock(unsigned int numeroBloco, unsigned char qtdTransaco
     return bloco;
 }
 
-BlocoMinerado mineBlock(BlocoNaoMinerado block, unsigned char hashAnterior)
+BlocoMinerado mineBlock(BlocoNaoMinerado block, unsigned char *hashAnterior)
 {
     unsigned char hash[SHA256_DIGEST_LENGTH]; // vetor que armazenará o resultado do hash. Tamanho definido pela libssl
     SHA256((unsigned char *)&block, sizeof(BlocoNaoMinerado), hash);
@@ -92,38 +92,135 @@ void printMinedBlock(BlocoMinerado bloco)
     }
 }
 
+void imprimeMenu(int *opcao)
+{
+    printf("1 - Continuar mineração\n");
+    printf("2 - Listar informações do bloco\n");
+    printf("3 - Quantidade de bitcoins\n");
+    printf("4 - Endereco com mais bitcoins\n");
+    printf("5 - Enderecos com mais bitcoins\n");
+    printf("6 - Sair\n");
+    scanf("%d", opcao);
+}
+
+void continueAndClear()
+{
+    printf("\nPressione enter para continuar...");
+    getchar();
+    getchar();
+    system("clear");
+}
+
+void mine(unsigned char *hashAnterior, int *numeroBloco, MTRand randNumber, BlocoMinerado blocoEspera)
+{
+    printf("Hash anterior: ");
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        printf("%02x", hashAnterior[i]);
+    }
+    printf("\n");
+    unsigned char qtdTransacoes = (unsigned char)(1 + (genRandLong(&randNumber) % 61));
+    BlocoNaoMinerado blocoAMinerar = createBlock(*numeroBloco, qtdTransacoes, hashAnterior, randNumber);
+    // printUnminedBlock(blocoAMinerar);
+    clock_t begin = clock();
+    BlocoMinerado blocoMinerado = mineBlock(blocoAMinerar, hashAnterior);
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Tempo gasto: %.5f s\n\n\n\n", time_spent);
+    printMinedBlock(blocoMinerado);
+    // copia o hash do bloco minerado para o hash anterior
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        hashAnterior[i] = blocoMinerado.hash[i];
+    }
+    if (blocoEspera.hash[0] == -1)
+    {
+        blocoEspera = blocoMinerado;
+    }
+    else
+    {
+        // append both blocks to file
+        FILE *fp;
+        fp = fopen("blockchain.bin", "ab");
+        fwrite(&blocoEspera, sizeof(BlocoMinerado), 1, fp);
+        fwrite(&blocoMinerado, sizeof(BlocoMinerado), 1, fp);
+        fclose(fp);
+        blocoEspera.hash[0] = -1;
+    }
+
+    printf("\n\n\n\n\n");
+    (*numeroBloco)++;
+}
+
 int main(int argc, char *argv[])
 {
     // Initialize the random number generator
     MTRand randNumber = seedRand(1234567); // objeto gerador com semente 1234567
-    int numeroBloco = 1;
-    // create a previous hash for the first block
+
     unsigned char hashAnterior[SHA256_DIGEST_LENGTH];
-    memset(hashAnterior, 0, SHA256_DIGEST_LENGTH);
-    while (1)
+    FILE *file;
+    file = fopen("blockchain.bin", "rb");
+
+    // get last block number
+    BlocoMinerado lastBlock;
+    int lastBlockNumber;
+    if (file)
     {
-        printf("Hash anterior: ");
-        for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+        while (fread(&lastBlock, sizeof(BlocoMinerado), 1, file))
         {
-            printf("%02x", hashAnterior[i]);
+            lastBlockNumber = lastBlock.bloco.numero;
+            for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+            {
+                hashAnterior[i] = lastBlock.hash[i];
+            }
         }
-        printf("\n");
-        unsigned char qtdTransacoes = (unsigned char)(1 + (genRandLong(&randNumber) % 61));
-        BlocoNaoMinerado blocoAMinerar = createBlock(numeroBloco, qtdTransacoes, hashAnterior, randNumber);
-        // printUnminedBlock(blocoAMinerar);
-        clock_t begin = clock();
-        BlocoMinerado blocoMinerado = mineBlock(blocoAMinerar, &hashAnterior);
-        clock_t end = clock();
-        double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-        printf("Tempo gasto: %.5f s\n\n\n\n", time_spent);
-        printMinedBlock(blocoMinerado);
-        // copia o hash do bloco minerado para o hash anterior
-        for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    }
+    else
+    {
+        lastBlockNumber = 0;
+        memset(hashAnterior, 0, SHA256_DIGEST_LENGTH);
+    }
+    int numeroBloco = lastBlockNumber + 1;
+    BlocoMinerado blocoEspera;
+    blocoEspera.hash[0] = -1;
+    // create a previous hash for the first block
+    int opcao = 0;
+    imprimeMenu(&opcao);
+    while (opcao != 6)
+    {
+        switch (opcao)
         {
-            hashAnterior[i] = blocoMinerado.hash[i];
+        case 1:
+            while(1){
+                mine(hashAnterior, &numeroBloco, randNumber, blocoEspera);
+            }
+            break;
+        case 2:
+            int numeroBloco;
+            printf("\nDigite o numero do bloco: ");
+            scanf("%d", &numeroBloco);
+            FILE *fp;
+            fp = fopen("blockchain.bin", "rb");
+            BlocoMinerado bloco;
+            while (fread(&bloco, sizeof(BlocoMinerado), 1, fp))
+            {
+                if (bloco.bloco.numero == numeroBloco)
+                {
+                    printMinedBlock(bloco);
+                    break;
+                }
+            }
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
         }
-        printf("\n\n\n\n\n");
-        numeroBloco++;
+        continueAndClear();
+        opcao = 0;
+        imprimeMenu(&opcao);
     }
     return 0;
 }
